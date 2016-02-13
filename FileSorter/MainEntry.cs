aee38a0b279ec0249cf075be5fa86a39a097a177
mzhang <zhang_xiaofeng@hotmail.com>
2016-02-13 14:08:10 +1000

    add more error messages

--- FileSorter/MainEntry.cs
+++ FileSorter/MainEntry.cs
@@ -10,23 +10,30 @@ namespace FileSorter
     {
         private static void Main(string[] args)
         {
-            if(args.Length == 0)
+            if (args.Length == 0)
             {
                 ShowUsage();
                 return;
             }
-           string filename = args[0];
-           if (!File.Exists(filename))
-           {
-               ShowErrorMessage();
-               return;
-           }
+            string filename = args[0];
+            if (!File.Exists(filename))
+            {
+                ShowErrorMessage();
+                return;
+            }
 
-           var fileSorter = new ScoreSorter(filename);
-           fileSorter.ReadInput();
-           fileSorter.WriteOutputFile();
+            var fileSorter = new ScoreSorter(filename);
+            if (!fileSorter.ReadInput())
+            {
+                ShowOperationErrorMessage();
+            }
+
+            if (!fileSorter.WriteOutputFile())
+            {
+                ShowOperationErrorMessage();
+            }
         }
-        
+
         static void ShowUsage()
         {
             string instruction = "Missing input file name\n Usage: FileSorter inputfilename";
@@ -37,7 +44,13 @@ namespace FileSorter
             string errorMessage = "Failed to open the input file\n Usage: FileSorter inputfilename";
             Console.WriteLine(errorMessage);
         }
+        static void ShowOperationErrorMessage()
+        {
+            string errorMessage = "The operation failed. Please check your input file format and check again.\n" +
+                                  "User score line should be in the format of lastname, firstname, score";
+            Console.WriteLine(errorMessage);
+        }
 
-        
+
     }
 }
